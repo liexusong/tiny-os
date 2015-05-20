05d961c6585f4e824856b2a878b5d75d65173fa3
unknown <liexusong@qq.com>
2015-05-21 00:08:17 +0800

    first commit

---
+++ include/kprintf.h
@@ -0,0 +1,10 @@
+#ifndef KPRINTF_H
+#define KPRINTF_H
+
+#include <scr.h>
+
+enum KP_LEVEL {KPL_DUMP, KPL_PANIC};
+
+void kprintf(enum KP_LEVEL, const char *fmt, ...);
+
+#endif
--- pm/README.md
+++
@@ -1,7 +0,0 @@
-
-How to:
-=======
-```shell
-$ nasm pm.asm -o pm.bin
-$ dd if=pmtest1.bin of=a.img bs=512 count=1 conv=notrunc
-```
